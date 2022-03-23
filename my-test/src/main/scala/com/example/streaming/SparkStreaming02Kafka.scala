49d6bfd22df828b4eb0f5d39593b11d89fd751e4
yaokai <yaokai@ushareit.com>
2022-03-23 16:37:20 +0800

    spark streaming kafka

--- my-test/src/main/scala/com/example/streaming/SparkStreaming02Kafka.scala
+++ my-test/src/main/scala/com/example/streaming/SparkStreaming02Kafka.scala
@@ -5,22 +5,21 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-/**
- * nc -lk 9999
- */
 object SparkStreaming02Kafka {
   def main(args: Array[String]): Unit = {
     //创建环境对象
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingKafka")
     val ssc = new StreamingContext(sparkConf, Seconds(60))
     val kafkaPara = Map[String, Object](
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "test.sz-short-video.sz-item.shareit-cassandra-test.sg2.stream:9092",
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "",
+      //消费者组
       ConsumerConfig.GROUP_ID_CONFIG -> "hello_group",
       "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
       "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
     )
     val kafkaDataDS = KafkaUtils.createDirectStream(ssc,
       LocationStrategies.PreferConsistent,
+      //topic = whelloworld
       ConsumerStrategies.Subscribe[String, String](Set("whelloworld"), kafkaPara)
     )
     kafkaDataDS.map(_.value()).print()

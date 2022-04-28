ef8763bfe8a633c9f6065bc23e18073944a3fe34
yaokai <yaokai@ushareit.com>
2022-04-28 16:22:12 +0800

    读parquet

--- my-test/src/main/scala/com/example/test/ReadParquet.scala
+++ my-test/src/main/scala/com/example/test/ReadParquet.scala
@@ -9,12 +9,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object ReadParquet extends Logging{
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder().master("local[1]").appName(this.getClass.getName).getOrCreate()
-    val df1: DataFrame = spark.read.parquet("part-0-2")
-    val df2: DataFrame = spark.read.parquet("part-0-3")
-    val df4: DataFrame = spark.read.parquet("part-0-4")
-    val df5: DataFrame = spark.read.parquet("part-0-5")
-    logWarning("count1 = "+df1.count()+",count2 = "+df2.count())
-    logWarning("count4 = "+df4.count()+",count5 = "+df5.count())
+    val df1: DataFrame = spark.read.parquet("/Users/shareit/Downloads/tmp/output/dt=20220424")
+    logWarning("count1 = "+df1.count())
     spark.stop()
   }
 }
